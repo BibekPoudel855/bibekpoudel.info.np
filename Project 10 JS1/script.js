5d8c5fbf34673232ee480440bfaff8212537939d
Bibek Poudel <bibekpoudel855@gmail.com>
2024-01-22 18:55:49 +0545

    game added

---
+++ Project 10 JS1/script.js
@@ -0,0 +1,113 @@
+let body = document.querySelector("body");
+
+// Dark/Light Mode
+function darkLightMode() {
+  let modeButton = document.querySelector(".mode-button");
+  let allModeButtons = document.querySelectorAll(".all-mode-button");
+  let currentMode = "light";
+  modeButton.addEventListener("click", () => {
+    if (currentMode === "light") {
+      body.classList.add("dark");
+      body.classList.remove("light");
+
+      for (const classes of allModeButtons) {
+        classes.classList.add("light-button");
+        classes.classList.remove("dark-button");
+      }
+      modeButton.innerHTML = "Light Mode";
+      currentMode = "dark";
+    } else {
+      body.classList.add("light");
+      body.classList.remove("dark");
+
+      for (const classes of allModeButtons) {
+        classes.classList.add("dark-button");
+        classes.classList.remove("light-button");
+      }
+      modeButton.innerHTML = "Dark Mode";
+      currentMode = "light";
+    }
+  });
+}
+darkLightMode();
+// main game logic
+let buttons = document.querySelectorAll(".boxes");
+let resetButton = document.querySelector(".reset-button");
+let winnerMessagePortal=document.querySelector(".winner-message-portal");
+let winnerH4=document.querySelector("#winner-h4");
+let newGameButton=document.querySelector("#new-game-button");
+
+let turnO = true;
+buttons.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO === true) {
+      // player o
+      box.innerHTML = "O";
+      turnO = false;
+    } else {
+      // player x
+      box.innerHTML = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+
+//conditon for winnner
+const winningPattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 4, 6],
+  [2, 5, 8],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+//winner message showing
+function winnerMessage(winner){
+  winnerH4.innerHTML=`Winner is ${winner}`;
+  winnerMessagePortal.classList.remove("hide-message");
+}
+// to disable button when game ends
+function disableButton(){
+  for (const button of buttons) {
+    button.disabled=true;
+  }
+}
+//checking winner by some condn
+function checkWinner() {
+  for (let pattern of winningPattern) {
+    let pos1=buttons[pattern[0]].innerText;
+    let pos2=buttons[pattern[1]].innerText;
+    let pos3=buttons[pattern[2]].innerText;
+    if (pos1 !=="" && pos2!==""&pos3!=="") {
+      if (pos1===pos2 && pos2===pos3) {
+        winnerMessage(pos1);
+        disableButton();
+      }
+    }
+  }
+}
+
+// to  enable button
+function enableButton() {
+  for (const button of buttons) {
+    button.disabled=false;
+  }
+}
+// reset and new game button 
+function resetNewGame() {
+  turnO=true;
+  for (const button of buttons) {
+    button.innerText="";
+  }
+  winnerMessagePortal.classList.add("hide-message");
+  enableButton();
+}
+// after pressing reseting and new game button
+newGameButton.addEventListener("click",resetNewGame)
+resetButton.addEventListener("click",resetNewGame)
+
