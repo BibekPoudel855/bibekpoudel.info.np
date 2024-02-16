df9fd2200fc5f89338ce40ff864aa77327276f8a
Bibek Poudel <bibekpoudel855@gmail.com>
2024-02-16 10:56:39 +0545

    changes in netflix js and media query

---
+++ Project 9/script.js
@@ -0,0 +1,38 @@
+let faqBoxes=document.querySelectorAll(".faq-inner-container");
+let faqAns=document.querySelectorAll(".faq-ans");
+console.log(faqBoxes);
+faqBoxes.forEach((box)=>{
+    box.addEventListener("click",(e)=>{
+    let contChild=box.childNodes;
+    let qnsChlid=contChild[1].childNodes;
+    let i=qnsChlid[3];
+    i.classList.toggle("js-rotate");
+    contChild[3].classList.toggle("js-passive");
+    });
+});
+
+///2nd method
+////////////
+function methodIIForFaq(){
+    // not called 
+    for (let i = 0; i < faqBoxes.length; i++) {
+        faqBoxes[i].addEventListener("click",(e)=>{
+            faqAns[i].classList.toggle("js-passive");
+        });
+    };
+}
+function methodIII() {
+    // 1 method
+// not calleddd
+// remove any one method 
+// we are doing this method by selecting childnodess
+faqBoxes.forEach((box)=>{
+    let i=box.querySelector("i");
+    box.addEventListener("click",(e)=>{
+    i.classList.toggle("js-rotate");
+    let ans=box.childNodes;
+    console.log();
+    ans[3].classList.toggle("js-passive");
+        });
+    });
+}
