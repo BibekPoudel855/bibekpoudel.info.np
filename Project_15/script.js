c73ee135678de92eaba09fa8965885e9f596dc18
Bibek Poudel <bibekpoudel855@gmail.com>
2024-06-30 19:19:10 +0545

    currency-converter-swap-button

--- Project_15/script.js
+++ Project_15/script.js
@@ -1,18 +1,20 @@
 // url for api
 const apiKey = "076b46e31b493c879d35fb7e";
-// url structure and source 
+// url structure and source
 //https://www.exchangerate-api.com/
 // const BASE_URL =`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
 const BASE_URL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest`;
 //selecting the elements
 let selectsArray = document.querySelectorAll(".drop-down select");
 let convertButton = document.querySelector("#convert-button");
+let swapCont=document.querySelector(".mid-icon-cont");
 let fromInput = document.querySelector("#from-input");
 let toInput = document.querySelector("#to-input");
 let fromSelect = document.querySelector("#from-select");
 let toSelect = document.querySelector("#to-select");
 let paraMessage=document.querySelector("#para-message");
 let dateSpan = document.querySelector("#para-date span");
+
 // main code
 
 //event to load when page open
@@ -46,6 +48,8 @@ function byDefaultOptionSelection(select, key) {
     select.value = "NPR";
   }
 }
+
+
 //change flag with change in select
 function changeFlagWithChangeInOption(e) {
   let currencyCode = e.target.value;
@@ -54,6 +58,16 @@ function changeFlagWithChangeInOption(e) {
   let flagIMG = e.target.parentElement.querySelector("img");
   flagIMG.src = newImgSRC;
 }
+//swap event
+swapCont.addEventListener("click",(e)=>{
+  let tempForSwap=fromSelect.value;
+  fromSelect.value=toSelect.value;
+  toSelect.value=tempForSwap;
+  let fromImgSrc = fromSelect.parentElement.querySelector("img").src;
+  let toImgSrc=toSelect.parentElement.querySelector("img").src;
+  document.querySelector("#from-img").src=toImgSrc;
+  document.querySelector("#to-img").src=fromImgSrc;
+});
 // button event and making userfrendly
 convertButton.addEventListener("click", (e) => {
   buttonEvent(e);
@@ -99,7 +113,6 @@ async function api(fromInputVal) {
 toInput.value = finalConvertedAmount;
 paraMessage.innerHTML=`<span>1</span> ${fromSelectVal.toUpperCase()} = <span>${rate}</span> ${toSelectVal}`;
 }
-
 
 // change date in last paragraph
 function changeDate() {
