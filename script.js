20260bb3eb89f11f125ed8a3e5606f8e24ce8906
Bibek Poudel <bibekpoudel855@gmail.com>
2024-08-31 10:52:28 +0545

    added search functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Selections
 let mainContainer=document.querySelector(".main-container");
+let searchBox=document.querySelector("#nav-search");
+let searchButtonBigScreen=document.querySelector("#search-for-big-scr");
 // main code
 let clutter='';
 addStructure();
@@ -19,4 +21,36 @@ function addStructure() {
 }
 function addClutterToPage(clutter) {
     mainContainer.innerHTML=clutter;
+}
+
+// Search function
+function searchProjects(query) {
+    const lowerCaseQuery = query.toLowerCase();
+    return projectData.filter(project => {
+        return (
+            project.projectName.toLowerCase().includes(lowerCaseQuery)||
+            project.projectDescription.toLowerCase().includes(lowerCaseQuery) ||
+            project.projectCategory.toLowerCase().includes(lowerCaseQuery)
+        )
+    });
+}
+
+searchBox.addEventListener("input",()=>{
+    let query = searchBox.value;
+    const results = searchProjects(query);
+    addStructureForSearchedData(results);
+});
+function addStructureForSearchedData(results) {
+    clutter='';
+    results.forEach((data)=>{
+        clutter+=`
+        <div class="card">
+                <img src="${data.projectImg}" alt="img">
+                <h1><a href="${data.projectLink}" target="_blank">${data.projectName}</a></h1>
+                <p>${data.projectDescription}</p>
+                <p class="last-updated">Last Updated ${data.projectChangesDate}</p>
+            </div>
+        `;
+    });
+    addClutterToPage(clutter);
 }
