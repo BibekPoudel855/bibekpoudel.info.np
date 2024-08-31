e1df9dc01276b5ab5d56bbdc94fd60d3c4ad6691
Bibek Poudel <bibekpoudel855@gmail.com>
2024-08-31 09:27:23 +0545

    updated project page as home page

---
+++ projectData.js
@@ -0,0 +1,114 @@
+let projectData = [
+    {
+        projectName: "Weather",
+        projectImg: "/Home-Page-Img/weather.png",
+        projectLink: "/Project_16/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-07-13"
+    },
+    {
+        projectName: "bibek inc. news",
+        projectImg: "/Home-Page-Img/news.jpg",
+        projectLink: "/Project_7/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-07-06"
+    },
+    {
+        projectName: "Currency Converter",
+        projectImg: "/Home-Page-Img/currency_converter.jpg",
+        projectLink: "/Project_15/index.html",
+        projectDescription: "Currency Converter App made by using Exchange Rate API and language used: HTML, CSS, and JS. You Can Find Source in My GitHub Account. If the app does not Work, Download Source Code and run it on your device.",
+        projectChangesDate: "2024-06-30"
+    },
+    {
+        projectName: "Alaram Clock",
+        projectImg: "/Home-Page-Img/alaram-clock.jpeg",
+        projectLink: "/Project_14/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-06-06"
+    },
+    {
+        projectName: "TO-DO App",
+        projectImg: "/Home-Page-Img/project_13_thumbnail.png",
+        projectLink: "/Project_13/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-05-14"
+    },
+    {
+        projectName: "Password Generator",
+        projectImg: "/Home-Page-Img/password-gen.jpeg",
+        projectLink: "/Project_12/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-03-23"
+    },
+    {
+        projectName: "Rock-Paper-Scissor",
+        projectImg: "/Home-Page-Img/rockPaperScissor.png",
+        projectLink: "/Project_11/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-02-02"
+    },
+    {
+        projectName: "TIC-TAC-TOE",
+        projectImg: "/Home-Page-Img/tictactoe.jpg",
+        projectLink: "/Project 10 JS1/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-01-27"
+    },
+    {
+        projectName: "Netflix",
+        projectImg: "/Home-Page-Img/netflix.jpg",
+        projectLink: "/Project 9/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-01-19"
+    },
+    {
+        projectName: "Amazon",
+        projectImg: "/Home-Page-Img/amazon.jpg",
+        projectLink: "/Project/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2023-12-02"
+    },
+    {
+        projectName: "UltraEdit",
+        projectImg: "/Home-Page-Img/ultraedit.png",
+        projectLink: "/Project_8/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2023-12-02"
+    },
+    {
+        projectName: "Adidas",
+        projectImg: "/Home-Page-Img/adidas.png",
+        projectLink: "/Project_4/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2023-12-02"
+    },
+    {
+        projectName: "Icm",
+        projectImg: "/Home-Page-Img/icm.png",
+        projectLink: "/Project_2/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-02-02"
+    },
+    {
+        projectName: "CSS Grid",
+        projectImg: "/Home-Page-Img/cssgrid.png",
+        projectLink: "/Project_5/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-02-02"
+    },
+    {
+        projectName: "Landing Page",
+        projectImg: "/Home-Page-Img/landingpage.png",
+        projectLink: "/Project_3/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-02-02"
+    },
+    {
+        projectName: "FAQ",
+        projectImg: "/Home-Page-Img/faq.jpg",
+        projectLink: "/Project_6/index.html",
+        projectDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nobis, enim quia officiis nemo, obcaecati quod vel cupiditate reiciendis accusamus odio aut delectus numquam quibusdam repudiandae impedit, laboriosam error blanditiis.",
+        projectChangesDate: "2024-02-02"
+    }
+];
--- script.js
+++ script.js
@@ -1,45 +1,22 @@
-// Sticky Navigation Menu JS Code
-let nav = document.querySelector("nav");
-let scrollBtn = document.querySelector(".scroll-button a");
-console.log(scrollBtn);
-let val;
-window.onscroll = function() {
-  if(document.documentElement.scrollTop > 20){
-    nav.classList.add("sticky");
-    scrollBtn.style.display = "block";
-  }else{
-    nav.classList.remove("sticky");
-    scrollBtn.style.display = "none";
-  }
-
+// Selections
+let mainContainer=document.querySelector(".main-container");
+// main code
+let clutter='';
+addStructure();
+function addStructure() {
+    projectData.forEach((data)=>{
+        console.log(data.projectLink);
+        clutter+=`
+        <div class="card">
+                <img src="${data.projectImg}" alt="img">
+                <h1><a href="${data.projectLink}" target="_blank">${data.projectName}</a></h1>
+                <p>${data.projectDescription}</p>
+                <p class="last-updated">Last Updated ${data.projectChangesDate}</p>
+            </div>
+        `;
+    });
+    addClutterToPage(clutter);
 }
-
-// Side NavIgation Menu JS Code
-let body = document.querySelector("body");
-let navBar = document.querySelector(".navbar");
-let menuBtn = document.querySelector(".menu-btn");
-let cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = function(){
-  navBar.classList.add("active");
-  menuBtn.style.opacity = "0";
-  menuBtn.style.pointerEvents = "none";
-  body.style.overflow = "hidden";
-  scrollBtn.style.pointerEvents = "none";
-}
-cancelBtn.onclick = function(){
-  navBar.classList.remove("active");
-  menuBtn.style.opacity = "1";
-  menuBtn.style.pointerEvents = "auto";
-  body.style.overflow = "auto";
-  scrollBtn.style.pointerEvents = "auto";
-}
-
-// Side Navigation Bar Close While We Click On Navigation Links
-let navLinks = document.querySelectorAll(".menu li a");
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click" , function() {
-    navBar.classList.remove("active");
-    menuBtn.style.opacity = "1";
-    menuBtn.style.pointerEvents = "auto";
-  });
+function addClutterToPage(clutter) {
+    mainContainer.innerHTML=clutter;
 }
