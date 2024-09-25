6338214d789cf8ac4e349c4ca5e30ad16aa0ce43
Bibek Poudel <bibekpoudel855@gmail.com>
2024-09-25 21:19:50 +0545

    test

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Selections
-let mainContainer=document.querySelector(".main-container");
-let searchBox=document.querySelector("#nav-search");
-let searchButtonBigScreen=document.querySelector("#search-for-big-scr");
-let resultNotFound=document.querySelector(".result-not-found");
+let mainContainer = document.querySelector(".main-container");
+let searchBox = document.querySelector("#nav-search");
+let searchButtonBigScreen = document.querySelector("#search-for-big-scr");
+let resultNotFound = document.querySelector(".result-not-found");
 
 // main code
-let clutter='';
+let clutter = "";
 addStructure();
 function addStructure() {
-    projectData.forEach((data)=>{
-        clutter+=`
+  projectData.forEach((data) => {
+    clutter += `
         <div class="card">
                 <img src="${data.projectImg}" alt="img">
                 <h1><a href="${data.projectLink}" target="_blank">${data.projectName}</a></h1>
@@ -17,35 +17,35 @@ function addStructure() {
                 <p class="last-updated">Last Updated ${data.projectChangesDate}</p>
             </div>
         `;
-    });
-    addClutterToPage(clutter);
+  });
+  addClutterToPage(clutter);
 }
 function addClutterToPage(clutter) {
-    mainContainer.innerHTML=clutter;
+  mainContainer.innerHTML = clutter;
 }
 
 // Search function
 function searchProjects(query) {
-    const lowerCaseQuery = query.toLowerCase();
-    return projectData.filter(project => {
-        return (
-            project.projectName.toLowerCase().includes(lowerCaseQuery)||
-            project.projectDescription.toLowerCase().includes(lowerCaseQuery) ||
-            project.projectCategory.toLowerCase().includes(lowerCaseQuery)
-        )
-    });
+  const lowerCaseQuery = query.toLowerCase();
+  return projectData.filter((project) => {
+    return (
+      project.projectName.toLowerCase().includes(lowerCaseQuery) ||
+      project.projectDescription.toLowerCase().includes(lowerCaseQuery) ||
+      project.projectCategory.toLowerCase().includes(lowerCaseQuery)
+    );
+  });
 }
 
-searchBox.addEventListener("input",()=>{
-    let query = searchBox.value;
-    const results = searchProjects(query);
-    console.log(results);
-    addStructureForSearchedData(results);
+searchBox.addEventListener("input", () => {
+  let query = searchBox.value;
+  const results = searchProjects(query);
+  console.log(results);
+  addStructureForSearchedData(results);
 });
 function addStructureForSearchedData(results) {
-    clutter='';
-    results.forEach((data)=>{
-        clutter+=`
+  clutter = "";
+  results.forEach((data) => {
+    clutter += `
         <div class="card">
                 <img src="${data.projectImg}" alt="img">
                 <h1><a href="${data.projectLink}" target="_blank">${data.projectName}</a></h1>
@@ -53,15 +53,15 @@ function addStructureForSearchedData(results) {
                 <p class="last-updated">Last Updated ${data.projectChangesDate}</p>
             </div>
         `;
-    });
-// for to make if search not found then show all items
-    if (clutter=='') {
-        //for to show result-not-found
-        resultNotFound.style.display="block";
-        addStructure();
-    }else{
-        //for to hide result-not-found
-        resultNotFound.style.display="none";
-        addClutterToPage(clutter);
-    }
+  });
+  // for to make if search not found then show all items
+  if (clutter == "") {
+    //for to show result-not-found
+    resultNotFound.style.display = "block";
+    addStructure();
+  } else {
+    //for to hide result-not-found
+    resultNotFound.style.display = "none";
+    addClutterToPage(clutter);
+  }
 }
