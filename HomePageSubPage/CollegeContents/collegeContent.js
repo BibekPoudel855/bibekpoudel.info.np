dd104ec0192f2967812b93d1d8022c2baa3373ae
Bibek Poudel <bibekpoudel855@gmail.com>
2025-01-11 10:10:58 +0545

    iscsw

--- HomePageSubPage/CollegeContents/collegeContent.js
+++ HomePageSubPage/CollegeContents/collegeContent.js
@@ -1,9 +1,9 @@
-let isButton= document.querySelector(".open-is-cw");
-console.log(isButton);
+// let isButton= document.querySelector(".open-is-cw");
+// console.log(isButton);
 
-isButton.addEventListener("click",()=>{
-    let pswfield= prompt("enter password given by bibek");
-    if(pswfield=="12345"){
-        window.open("./../isCourseWork/Course_Work1_Information_System/index.html");
-    }
-});
+// isButton.addEventListener("click",()=>{
+//     let pswfield= prompt("enter password given by bibek");
+//     if(pswfield=="12345"){
+//         window.open("./../isCourseWork/Course_Work1_Information_System/index.html");
+//     }
+// });
